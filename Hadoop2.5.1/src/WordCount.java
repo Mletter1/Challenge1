7df49b0be042f412b9b9d332b9b39cdb17d5eb31
Matthew Letter <mletter1@unm.edu>
2014-10-04 13:35:45 -0600

    modulated hadoop 2.5.1

--- Hadoop2.5.1/src/WordCount.java
+++ Hadoop2.5.1/src/WordCount.java
@@ -26,10 +26,12 @@ public class WordCount {
             extends Mapper<Object, Text, Text, IntWritable>{
 
         private final static IntWritable one = new IntWritable(1);
+
         private Text word = new Text();
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
+
             StringTokenizer itr = new StringTokenizer(value.toString());
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
